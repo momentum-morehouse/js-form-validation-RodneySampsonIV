c0375821efedbfd42e61a77ca37d9371957529c6
Rodney Sampson <rodneysampsoniv@gmail.com>
2020-06-12 22:39:50 +0000

    adds functions

--- script.js
+++ script.js
@@ -1,1 +1,32 @@
 console.log('Add validation!');
+
+
+
+function emptyFields() {
+    let customer = document.getElementById('#name');
+    let caryear = document.getElementById('#car-year');
+    let carmake = document.getElementById('#car-make');
+    let carmodel = document.getElementById('#car-model');
+    let startdate = document.getElementById('#start-date');
+    let amtdays = document.getElementById('#days');
+    let cc = document.getElementById('#credit-card-field');
+    let cCVV = document.getElementById('#cvv-field');
+    let ccDate = document.getElementById('#expiration-field')
+    return true;
+}
+emptyFields();
+
+
+function validatior() {
+    if (customer.value.length == 0) {
+        document.body.style.backgroundColor = "red";
+    }
+
+    else {
+        alert("message");
+        return false;
+
+    }
+    return true;
+}
+validatior(customer)
