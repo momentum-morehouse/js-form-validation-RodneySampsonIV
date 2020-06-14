1a6cc0967e139a51d188e4c1d8c3f3232ab66db9
Rodney Sampson <rodneysampsoniv@gmail.com>
2020-06-14 04:34:17 +0000

    Completes step 1

--- script.js
+++ script.js
@@ -1,32 +1,46 @@
 console.log('Add validation!');
 
+let parkingForm = document.getElementById("parking-form");
 
+parkingForm.addEventListener('submit', (event) => {
+    event.preventDefault();
+    validate();
+});
 
-function emptyFields() {
-    let customer = document.getElementById('#name');
-    let caryear = document.getElementById('#car-year');
-    let carmake = document.getElementById('#car-make');
-    let carmodel = document.getElementById('#car-model');
-    let startdate = document.getElementById('#start-date');
-    let amtdays = document.getElementById('#days');
-    let cc = document.getElementById('#credit-card-field');
-    let cCVV = document.getElementById('#cvv-field');
-    let ccDate = document.getElementById('#expiration-field')
-    return true;
-}
-emptyFields();
+function validate() {
+    let customerInput = document.getElementById('name');
+    let customerField = document.getElementById('name-field');
 
+    let carYearInput = document.getElementById('car-year');
+    let carMakeInput = document.getElementById('car-make');
+    let carModelInput = document.getElementById('car-model');
+    let carField = document.getElementById('car-field');
 
-function validatior() {
-    if (customer.value.length == 0) {
-        document.body.style.backgroundColor = "red";
+    let startDateInput = document.getElementById('start-date');
+    let startDateField = document.getElementById('start-date-field');
+
+    let amountOfDaysInput = document.getElementById('days');
+    let amountOfDaysField = document.getElementById('days-field');
+    
+    let creditCardInput = document.getElementById('credit-card');
+    let creditCardField = document.getElementById('credit-card-field');
+
+    let creditCardCVVInput = document.getElementById('cvv');
+    let creditCardCVVField = document.getElementById('cvv-field');
+	
+    let creditCardDateInput = document.getElementById('expiration');
+    let creditCardDateField = document.getElementById('expiration-field'); 
+
+    let inputs = [customerInput, carYearInput, carMakeInput, carModelInput, startDateInput, amountOfDaysInput, creditCardInput, creditCardCVVInput, creditCardDateInput];
+
+    let fields = [customerField, carField, carField, carField, startDateField, amountOfDaysField, creditCardField, creditCardCVVField, creditCardDateField, creditCardDateInput];
+
+    for (var index = 0; index < inputs.length; index++) {
+        if (inputs[index].value.length === 0) {
+            fields[index].className = "input-field input-invalid";
+        } else {
+        	fields[index].className = "input-field input-valid";
+		}
     }
-
-    else {
-        alert("message");
-        return false;
-
-    }
-    return true;
+	
 }
-validatior(customer)
